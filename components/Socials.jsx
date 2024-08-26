596cf7dd20189c7989e2a4ad5d2c6104ec0ec241
Shivbhan Kushwaha <108090221+ShivBhanKushWaha@users.noreply.github.com>
2024-08-26 18:42:45 +0530

    Update Socials.jsx

--- components/Socials.jsx
+++ components/Socials.jsx
@@ -12,7 +12,7 @@ import {
 
 const icons = [
     {
-        path: 'https://www.youtube.com/@ShivbhanKushwaha3051',
+        path: 'https://www.youtube.com/@shivbhantechadvisor',
         name:<RiYoutubeFill/>,
     },
     {
